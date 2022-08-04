50d21634be446ff7ead7b8122433771c8b41c342
Santiago1034 <107003145+Santiago1034@users.noreply.github.com>
2022-08-04 14:04:20 -0500

    Commit 1

---
+++ java/acordeon.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $('.p1').hide()
+    $('.p2').hide()
+    $('.p3').hide()
+    $('.logo').on('click',function(){
+    $('.p1').slideToggle()
+    $('.p2').slideToggle()
+    $('.p3').slideToggle();
+    
+    
+    });
+}) 
